93dd307114b554180c77951d2c917a3974001457
vijay singh <130821386+vijayrajput12@users.noreply.github.com>
2024-05-09 19:26:55 +0530

    first commit

---
+++ js/first.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.cart').click(function(){
+        $('.cart').hide()
+
+    })
+})
+
+$(document).ready(function(){
+    $('.cart1').click(function(){
+        alert('  your Product added Thanku😍')
+    })
+})
